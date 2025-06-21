717d084639ab5fb5ba3d29d30dd057520ee6f42b
adityakat16 <adityapkatti@gmail.com>
2025-06-21 21:52:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y wget gnupg unzip
+
+# Install Chrome
+apt-get update && apt-get install -y wget gnupg unzip curl
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
-apt-get install -y chromium-chromedriver
+
+# Install Chromedriver
+CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+')
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget -O chromedriver.zip "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+unzip chromedriver.zip
+mv chromedriver /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
