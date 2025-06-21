09093c5b8a4b37fd0aefe934ede4ea6d554f6992
adityakat16 <adityapkatti@gmail.com>
2025-06-21 11:17:53 +0530

    Update sele.py

--- sele.py
+++ sele.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import time
 import re
 import sys
