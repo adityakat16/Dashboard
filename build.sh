c4e18286fc616c459d26b2770383da0c9fa72acf
adityakat16 <adityapkatti@gmail.com>
2025-06-21 19:18:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 apt-get update && apt-get install -y wget gnupg unzip
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
+apt-get install -y chromium-chromedriver
