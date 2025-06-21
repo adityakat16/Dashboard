44d59c9befd424b9e777ea0c114959ea57c6fea3
adityakat16 <adityapkatti@gmail.com>
2025-06-21 23:35:22 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -12,7 +12,7 @@ CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+')
 CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
 wget -O chromedriver.zip "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
 unzip chromedriver.zip
-mv chromedriver /usr/bin/chromedriver
-chmod +x /usr/bin/chromedriver
+mv chromedriver /usr/local/bin/chromedriver
+chmod +x /usr/local/bin/chromedriver
 
 pip install -r requirements.txt
