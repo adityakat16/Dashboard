ca204577c53c401f7023878706d787a924795f0b
adityakat16 <adityapkatti@gmail.com>
2025-06-21 22:01:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
-echo "Checking Chrome install..."
-google-chrome --version || echo "Chrome not found"
+#!/usr/bin/env bash
 
-echo "Checking Chromedriver install..."
-ls -l /usr/bin/chromedriver || echo "Chromedriver not found"
+apt-get update && apt-get install -y wget gnupg unzip curl
+
+# Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+
+# Symlink Chrome to PATH
+ln -s /usr/bin/google-chrome /usr/local/bin/google-chrome
